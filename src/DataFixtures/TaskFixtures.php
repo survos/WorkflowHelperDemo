ad4e9229d10552951fa7df252f0b30ee2e86b572
Tac Tacelosky <tac@survos.com>
2018-08-12 08:17:16 -0400

    add fixtures, update README

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $task = new Task();
+        $task
+            ->setName("First Task");
+        $manager->persist($task);
+
+        $manager->flush();
+    }
+}
